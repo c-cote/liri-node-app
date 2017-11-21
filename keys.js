4f4e5932899eae1945d70c8a4a41dd8e798db07d
chris cote <kristoball01@gmail.com>
2017-11-20 19:40:53 -0800

    start of commands

---
+++ keys.js
@@ -0,0 +1,8 @@
+var Twitter = new Twitter(keys);
+
+module.exports = {
+    consumer_key: 'DUT2PEGQnw0SCVEx1hsZdiFGs',
+    consumer_secret: 'fD9TgLdm1OIpatSETFwjOsKxwqycsMCQUvJkhLQ6eV4p3XeaFP',
+    access_token_key: '932433394008408064-sW8BWhjwPd7xRhdNlicv1e7UMTeqxUr',
+    access_token_secret: 'jSRkgFIoUSOEf2aHv6SSxisPRCqirfm542pEpzvsLrDsp'
+  };
